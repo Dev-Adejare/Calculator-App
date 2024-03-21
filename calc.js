71cbfd6fde9b4722405a1f379e155d3bd4289363
Dev-Adejare <sodiqkehinde64@gmail.com>
2024-03-21 15:05:30 +0100

    update

---
+++ calc.js
@@ -0,0 +1,70 @@
+var screen = document.querySelector('#screen');
+    var btn = document.querySelectorAll('.btn');
+
+    for(item of btn)
+    {
+        item.addEventListener('click',(e)=>{
+          btntext= e.target.innerText;
+           
+            
+            if(btntext=='x')
+            {
+                btntext='*';
+            }
+
+            if(btntext=='÷')
+            {
+                btntext='/';
+            }
+            screen.value+=btntext;
+        });
+    }
+
+    function sin()
+    {
+        screen.value=Math.sin(screen.value);
+    }
+    function cos()
+    {
+        screen.value=Math.cos(screen.value);
+    }
+    function tan()
+    {
+        screen.value=Math.tan(screen.value);
+    }
+    function pow()
+    {
+        screen.value=Math.pow(screen.value,2);
+    }
+    function log()
+    {
+        screen.value=Math.log(screen.value,2);
+    }
+    function pi()
+    {
+        screen.value=3.14159265359;
+    }
+    function e()
+    {
+        screen.value=2.718281882846;
+    }
+
+    function fact()
+    {
+        var i, num, f;
+        f=1
+        num=screen.value;
+        for(i=1; i<=num; i++)
+        {
+         f=f*i;   
+        }
+        
+        i=i-i;
+
+        screen.value=f;
+    }
+
+    function backspc()
+    {
+        screen.value=screen.value.substr(0,screen.value.length-1);
+    }

40e4c96bb31dfbaad108a8c5475a56c151040e77
Dev-Adejare <sodiqkehinde64@gmail.com>
2024-03-21 15:27:16 +0100

    update

--- calc.js
+++ calc.js
@@ -24,26 +24,37 @@ var screen = document.querySelector('#screen');
     {
         screen.value=Math.sin(screen.value);
     }
+
     function cos()
     {
         screen.value=Math.cos(screen.value);
     }
+
     function tan()
     {
         screen.value=Math.tan(screen.value);
     }
+
     function pow()
     {
         screen.value=Math.pow(screen.value,2);
     }
+
     function log()
     {
         screen.value=Math.log(screen.value,2);
     }
+
+    function sqrt()
+    {
+        screen.value=Math.sqrt(screen.value,2);
+    }
+
     function pi()
     {
         screen.value=3.14159265359;
     }
+    
     function e()
     {
         screen.value=2.718281882846;
